4553e097a630cca78e89def83c89b480b17f7411
rlphilli <rlphillips@haverford.edu>
2017-03-13 04:54:17 -0400

    Wrote shuffling code

--- LearnSmiles.py
+++ LearnSmiles.py
@@ -1,11 +1,17 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.layers import LSTM
-from keras.optimizers import RMSprop
+from keras.layers.core import Lambda, Dropout
+from keras.optimizers import Adam
 from keras.utils.data_utils import get_file
 import numpy as np
 import random
 import sys
+import os
+
+from shuffle_smiles import 
+RUN_KEY = 'Test'
+SHUFFLE = True
 
 # Locate and read smiles strings from file
 SMILES_PATH = 'smiles.txt'
@@ -13,7 +19,24 @@ smiles_text = open(SMILES_PATH).read()
 
 # Prepare one-hot char representation
 chars = sorted(list(set(smiles_text)))
-char_indices = dict((c, i) for i, c in enumerate(chars))
+alphabet_size = len(chars)
+char_indices = dict((c, np.identity(alphabet_size)[i, :]) for i, c in enumerate(chars))
 indices_char = dict((i, c) for i, c in enumerate(chars))
 
 # Model setup (based on paper from MHS Segler Generating Focussed Molecule Libraries...)
+model = Sequential()
+Lambda(lambda x : char_indices[x], output_shape=(1, alphabet_size), arguments=None)
+model.add(LSTM(1024, input_shape=(1, alphabet_size)))
+model.add(Dropout(0.2))
+model.add(LSTM(1024, input_shape=(1, alphabet_size)))
+model.add(Dropout(0.2))
+model.add(LSTM(1024, input_shape=(1, alphabet_size)))
+model.add(Dropout(0.2))
+model.add(Dense(len(chars)))
+model.add(Activation('softmax'))
+
+# Use clipping norm of 5 to avoid exploding gradients
+optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+
+
+for i in range(100):
---
+++ shuffle_smiles.py
@@ -0,0 +1,25 @@
+from random import randint
+
+def fisherYatesShuffle(smiles_list):
+    n = len(smiles_list)
+    while (n > 1):
+        n = n - 1
+        k = randint(0, n+1)
+        tmp = smiles_list[k]
+        smiles_list[k] = smiles_list[n]
+        smiles_list[n] = tmp
+
+
+def shuffle_lines(source, destination=None):
+    """Randomly shuffle the lines of a file using Fisher-Yates and write to a new file."""
+    assert isinstance(source, str)
+    with open(source, 'r') as f:
+        smiles_list = f.readlines()
+        smiles_list = fisherYatesShuffle(smiles_list)
+
+        if destination is None:
+            destination = source
+
+        with open(source, 'w') as f2:
+            for line in smiles_list:
+                f2.write(line)
